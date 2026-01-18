c55c56601d633a43e92707a9b8ad7a4f4580ae53
n0reikas <nojusnoreika1@gmail.com>
2026-01-18 08:07:48 +0200

    change backend variables

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.MYSQLPORT;
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 app.use(cors());
